c1cc6e1d7f371546ab1aa3100676150ca87e9e08
AnotherMattWright <270914550+AnotherMattWright@users.noreply.github.com>
2026-04-09 16:19:28 +0100

    fix url on continue

--- test/views/AccessDeniedViewSpec.scala
+++ test/views/AccessDeniedViewSpec.scala
@@ -38,7 +38,7 @@ class AccessDeniedViewSpec extends SpecBase {
       doc.select("p").text must include(messages("accessDenied.p1"))
       val link = doc.select(".page-not-found-home-page-link a").attr("href")
 
-      link mustEqual "controllers.routes.IndexController.onPageLoad().url"
+      link mustEqual controllers.routes.IndexController.onPageLoad().url
 
     }
   }
