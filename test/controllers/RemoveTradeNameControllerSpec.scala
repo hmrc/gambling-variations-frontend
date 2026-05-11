333c9e06c561049bf1a3b2b1bc68940aaa7c1246
darrylmurphy <darrylmurphy@users.noreply.github.com>
2026-05-11 14:33:44 +0100

    DTR-fix: adjust code to businessType

--- test/controllers/RemoveTradeNameControllerSpec.scala
+++ test/controllers/RemoveTradeNameControllerSpec.scala
@@ -23,7 +23,6 @@ import navigation.{FakeNavigator, Navigator}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
-import pages.RemoveTradeNamePage
 import play.api.inject.bind
 import play.api.libs.json.Json
 import play.api.mvc.Call
