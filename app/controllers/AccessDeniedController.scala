6eed5490e88a9ff5e4d14e4c3a009b6c3b364a8b
AnotherMattWright <270914550+AnotherMattWright@users.noreply.github.com>
2026-05-12 11:13:22 +0100

    refactor to route through config

--- app/controllers/AccessDeniedController.scala
+++ app/controllers/AccessDeniedController.scala
@@ -20,17 +20,19 @@ import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.AccessDeniedView
+import config.FrontendAppConfig
 
 import javax.inject.Inject
 
 class AccessDeniedController @Inject() (
   override val messagesApi: MessagesApi,
   val controllerComponents: MessagesControllerComponents,
+  appConfig: FrontendAppConfig,
   view: AccessDeniedView
 ) extends FrontendBaseController
     with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = Action { implicit request =>
-    Ok(view())
+    Ok(view(appConfig.continueToSignIn))
   }
 }
