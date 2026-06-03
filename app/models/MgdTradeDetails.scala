09ef1c4117b7c1ae2f17eeb0aab92c9ed023af02
darrylmurphy <darrylmurphy@users.noreply.github.com>
2026-06-03 16:26:49 +0100

    DTR-5485: msg update

--- app/models/MgdTradeDetails.scala
+++ app/models/MgdTradeDetails.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package models
 
 import play.api.libs.json.{Json, OFormat}
@@ -5,14 +21,14 @@ import play.api.libs.json.{Json, OFormat}
 import java.time.LocalDate
 
 case class MgdTradeDetails(
-                            mgdRegNumber: String,
-                            isBusinessSeasonal: Option[Boolean],
-                            businessTradeClass: Option[BusinessTradeClass],
-                            businessActivityDesc: Option[String],
-                            previousMgdRegistratonNumbers: Option[Seq[String]],
-                            associatedMgdRegistratonNumbers: Option[Seq[String]],
-                            systemDate: Option[LocalDate]
-                          )
+  mgdRegNumber: String,
+  isBusinessSeasonal: Option[Boolean],
+  businessTradeClass: Option[BusinessTradeClass],
+  businessActivityDesc: Option[String],
+  previousMgdRegistrationNumbers: Option[Seq[String]],
+  associatedMgdRegistrationNumbers: Option[Seq[String]],
+  systemDate: Option[LocalDate]
+)
 
 object MgdTradeDetails {
   implicit val format: OFormat[MgdTradeDetails] =
