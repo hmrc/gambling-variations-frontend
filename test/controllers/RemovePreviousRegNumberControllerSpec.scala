0302a8e7ce19baa549fa3bba2f03cf858cb83805
AnotherMattWright <270914550+AnotherMattWright@users.noreply.github.com>
2026-06-05 15:32:44 +0100

    fixed controller spec

--- test/controllers/RemovePreviousRegNumberControllerSpec.scala
+++ test/controllers/RemovePreviousRegNumberControllerSpec.scala
@@ -108,7 +108,7 @@ class RemovePreviousRegNumberControllerSpec extends SpecBase with MockitoSugar {
       }
     }
 
-    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+    "must redirect to System Error for a GET if no existing data is found" in {
 
       val application = applicationBuilder(userAnswers = None).build()
 
@@ -118,11 +118,11 @@ class RemovePreviousRegNumberControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        redirectLocation(result).value mustEqual routes.SystemErrorController.onPageLoad().url
       }
     }
 
-    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+    "must redirect to System Error for a POST if no existing data is found" in {
 
       val application = applicationBuilder(userAnswers = None).build()
 
@@ -134,7 +134,7 @@ class RemovePreviousRegNumberControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+        redirectLocation(result).value mustEqual routes.SystemErrorController.onPageLoad().url
       }
     }
   }
