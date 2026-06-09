27e9544aeff7700831f2e65f94539063dbe8ff2a
Javed <131012491+javed-iqbal1@users.noreply.github.com>
2026-06-09 10:20:01 +0100

    DTR-4862 initial changes - page created with  change link todo

---
+++ app/controllers/CheckTradingDetailsController.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions.*
+
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import viewmodels.checkAnswers.*
+import viewmodels.checkAnswers.tradingdetails.*
+import viewmodels.govuk.summarylist.*
+import views.html.CheckTradingDetailsView
+
+class CheckTradingDetailsController @Inject()(
+                                               override val messagesApi: MessagesApi,
+                                               authorised: AuthorisedAction,
+                                               getData: DataRetrievalAction,
+                                               requireData: DataRequiredAction,
+                                               val controllerComponents: MessagesControllerComponents,
+                                               view: CheckTradingDetailsView
+                                             ) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad: Action[AnyContent] =
+    (authorised andThen getData andThen requireData) { implicit request =>
+
+      val list = SummaryListViewModel(
+        rows = Seq(
+          BusinessTradeClassSummary.row(request.userAnswers),
+          IsSeasonalBusinessSummary.row(request.userAnswers),
+        ).flatten
+      )
+
+      val listPreviousMgdRegNo = SummaryListViewModel(
+        rows = Seq(
+          PreviousRegistrationNumbersSummary.row(request.userAnswers)
+
+        ).flatten
+      )
+      val listAssociatedMgdRegNo = SummaryListViewModel(
+        rows = Seq(
+          AssociatedRegistrationNumbersSummary.row(request.userAnswers)
+
+        ).flatten
+      )
+
+      Ok(view(list, listPreviousMgdRegNo, listAssociatedMgdRegNo))
+    }
+}
