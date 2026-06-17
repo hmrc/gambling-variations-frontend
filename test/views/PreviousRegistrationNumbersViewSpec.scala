c2cf642cf013a53a3523675202c2d01821adb018
AnotherMattWright <270914550+AnotherMattWright@users.noreply.github.com>
2026-06-17 11:36:08 +0100

    view spec updated

--- test/views/PreviousRegistrationNumbersViewSpec.scala
+++ test/views/PreviousRegistrationNumbersViewSpec.scala
@@ -31,7 +31,7 @@ class PreviousRegistrationNumbersViewSpec extends SpecBase {
     val app = applicationBuilder().build()
 
     val view = app.injector.instanceOf[PreviousRegistrationNumbersView]
-    val formProvider = new AssociatedRegistrationNumbersFormProvider()
+    val formProvider = new PreviousRegistrationNumbersFormProvider()
     val form = formProvider()
 
     implicit val request: play.api.mvc.Request[?] = FakeRequest()
@@ -47,10 +47,10 @@ class PreviousRegistrationNumbersViewSpec extends SpecBase {
 
       val doc = Jsoup.parse(html.body)
 
-      doc.title             must include(messages("associatedRegistrationNumbers.title"))
-      doc.select("h1").text must include(messages("associatedRegistrationNumbers.heading"))
+      doc.title             must include(messages("previousRegistrationNumbers.title"))
+      doc.select("h1").text must include(messages("previousRegistrationNumbers.heading"))
 
-      doc.select(".associated-reg-number-XHM00000199").text must include("XHM00000199")
+      doc.select(".previous-reg-number-XHM00000199").text must include("XHM00000199")
     }
 
     "must show radio buttons when less than 3 numbers" in new Setup {
@@ -59,12 +59,12 @@ class PreviousRegistrationNumbersViewSpec extends SpecBase {
 
       val doc = Jsoup.parse(html.body)
 
-      doc.title             must include(messages("associatedRegistrationNumbers.title"))
-      doc.select("h1").text must include(messages("associatedRegistrationNumbers.heading"))
+      doc.title             must include(messages("previousRegistrationNumbers.title"))
+      doc.select("h1").text must include(messages("previousRegistrationNumbers.heading"))
 
-      doc.select(".associated-reg-number-ZIU00001218").text must include("ZIU00001218")
-      doc.select(".assoc-reg-radio-buttons").text           must include(messages("associatedRegistrationNumbers.yesLabel"))
-      doc.select(".assoc-reg-radio-buttons").text           must include(messages("associatedRegistrationNumbers.noLabel"))
+      doc.select(".previous-reg-number-ZIU00001218").text must include("ZIU00001218")
+      doc.select(".prev-reg-radio-buttons").text          must include(messages("previousRegistrationNumbers.yesLabel"))
+      doc.select(".prev-reg-radio-buttons").text          must include(messages("previousRegistrationNumbers.noLabel"))
     }
 
     "must show max limit message when 3 numbers are present" in new Setup {
@@ -73,10 +73,10 @@ class PreviousRegistrationNumbersViewSpec extends SpecBase {
 
       val doc = Jsoup.parse(html.body)
 
-      doc.title                                             must include(messages("associatedRegistrationNumbers.title"))
-      doc.select("h1").text                                 must include(messages("associatedRegistrationNumbers.heading"))
-      doc.select(".associated-reg-number-XHM00000199").text must include("XHM00000199")
-      doc.select(".assoc-reg-max-limit-reached").text       must include(messages("associatedRegistrationNumbers.hint"))
+      doc.title                                           must include(messages("previousRegistrationNumbers.title"))
+      doc.select("h1").text                               must include(messages("previousRegistrationNumbers.heading"))
+      doc.select(".previous-reg-number-XHM00000199").text must include("XHM00000199")
+      doc.select(".prev-reg-max-limit-reached").text      must include(messages("previousRegistrationNumbers.hint"))
     }
 
   }
