f2bded69fc08fb6f894eae7b59ce582dcf40aa62
benmasters0 <117096227+benmasters0@users.noreply.github.com>
2026-07-02 12:44:20 +0100

    dtr 6661

---
+++ test/forms/CorrespondenceAdditionalNameFormProviderSpec.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.behaviours.StringFieldBehaviours
+import org.scalacheck.Gen
+import play.api.data.FormError
+
+class CorrespondenceAdditionalNameFormProviderSpec extends StringFieldBehaviours {
+
+  private val requiredKey = "correspondenceAdditionalName.error.required"
+  private val lengthKey = "correspondenceAdditionalName.error.length"
+  private val invalidKey = "correspondenceAdditionalName.error.invalid"
+
+  private val maxLength = 100
+
+  private val form = new CorrespondenceAdditionalNameFormProvider()()
+
+  private val correspondenceAdditionalNameRegex = "^[a-zA-Z0-9\\-'\\s]+$"
+
+  ".value" - {
+
+    val fieldName = "value"
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      Gen.oneOf(
+        "foobar",
+        "validname",
+        "jonjones",
+      )
+    )
+
+    "bind strings longer than maximum length with length error" in {
+
+      val validLongString = "A" * (maxLength + 1)
+
+      val result = form.bind(
+        Map(fieldName -> validLongString)
+      )
+
+      result.errors must contain only
+        FormError(fieldName, lengthKey, Seq(maxLength))
+    }
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+
+    "not bind invalid characters" in {
+
+      val invalidValues = Seq(
+        "Test@",
+        "Hello#World",
+        "invalid!",
+        "name$123"
+      )
+
+      invalidValues.foreach { value =>
+
+        val result = form.bind(
+          Map(fieldName -> value)
+        )
+
+        result.errors must contain(
+          FormError(fieldName, invalidKey, Seq(correspondenceAdditionalNameRegex))
+        )
+      }
+    }
+  }
+}
