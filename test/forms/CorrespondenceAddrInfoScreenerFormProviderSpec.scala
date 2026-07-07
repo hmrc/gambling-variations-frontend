47ea6625884c2c08cd36b61240617d304db248a9
AnotherMattWright <270914550+AnotherMattWright@users.noreply.github.com>
2026-07-07 09:21:55 +0100

    quick fix

--- test/forms/CorrespondenceAddrInfoScreenerFormProviderSpec.scala
+++ test/forms/CorrespondenceAddrInfoScreenerFormProviderSpec.scala
@@ -21,7 +21,7 @@ import play.api.data.FormError
 
 class CorrespondenceAddrInfoScreenerFormProviderSpec extends BooleanFieldBehaviours {
 
-  val requiredKey = "correspondenceAddrAdditionalInfoScreener.error.required"
+  val requiredKey = "correspondenceAddrInfoScreener.error.required"
   val invalidKey = "error.boolean"
 
   val form = new CorrespondenceAddrInfoScreenerFormProvider()()
