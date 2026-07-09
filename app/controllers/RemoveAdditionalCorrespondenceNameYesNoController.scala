1f56910b0b6831ecb2a708d1bbd43504960e41e2
Javed Iqbal <131012491+javed-iqbal1@users.noreply.github.com>
2026-07-09 16:37:38 +0100

    DTR-6834 Remove correspondence name 2- Screen build (#120)
    DTR-6834 Remove correspondence name 2- Screen build

---
+++ app/controllers/RemoveAdditionalCorrespondenceNameYesNoController.scala
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions.*
+import forms.RemoveAdditionalCorrespondenceNameYesNoFormProvider
+
+import javax.inject.Inject
+import models.Mode
+import navigation.Navigator
+import pages.{AddCorrespondenceAdditionalNamePage, CorrespondenceAdditionalNamePage, CorrespondenceDetailsChangesPage, RemoveAdditionalCorrespondenceNameYesNoPage}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.RemoveAdditionalCorrespondenceNameYesNoView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class RemoveAdditionalCorrespondenceNameYesNoController @Inject() (
+  override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
+  navigator: Navigator,
+  authorise: AuthorisedAction,
+  getData: DataRetrievalAction,
+  requireData: CorrespondenceDetailsDataRequiredAction,
+  formProvider: RemoveAdditionalCorrespondenceNameYesNoFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  view: RemoveAdditionalCorrespondenceNameYesNoView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  private val form = formProvider()
+
+  def onPageLoad(mode: Mode): Action[AnyContent] =
+    (authorise andThen getData andThen requireData) { implicit request =>
+
+      request.userAnswers.get(CorrespondenceAdditionalNamePage) match {
+        case Some(name2) =>
+          val preparedForm =
+            request.userAnswers.get(RemoveAdditionalCorrespondenceNameYesNoPage) match {
+              case Some(value) => form.fill(value)
+              case None        => form
+            }
+
+          Ok(view(preparedForm, mode, name2))
+
+        case None =>
+          Redirect(routes.SystemErrorController.onPageLoad())
+      }
+    }
+
+  def onSubmit(mode: Mode): Action[AnyContent] =
+    (authorise andThen getData andThen requireData).async { implicit request =>
+
+      request.userAnswers.get(CorrespondenceAdditionalNamePage) match {
+
+        case Some(name2) =>
+          form
+            .bindFromRequest()
+            .fold(
+              formWithErrors =>
+                Future.successful(
+                  BadRequest(view(formWithErrors, mode, name2))
+                ),
+              value => {
+                val updatedAnswers =
+                  if (value) {
+                    for {
+                      updatedAnswers <- request.userAnswers.remove(CorrespondenceAdditionalNamePage)
+                      updatedAnswers <- updatedAnswers.set(AddCorrespondenceAdditionalNamePage, false)
+                      updatedAnswers <- updatedAnswers.set(RemoveAdditionalCorrespondenceNameYesNoPage, value)
+                      updatedAnswers <- updatedAnswers.set(CorrespondenceDetailsChangesPage, value)
+                    } yield updatedAnswers
+                  } else {
+                    request.userAnswers.set(RemoveAdditionalCorrespondenceNameYesNoPage, value)
+                  }
+
+                Future
+                  .fromTry(updatedAnswers)
+                  .flatMap { answers =>
+                    sessionRepository.set(answers).map { _ =>
+                      Redirect(
+                        navigator.nextPage(
+                          RemoveAdditionalCorrespondenceNameYesNoPage,
+                          mode,
+                          answers
+                        )
+                      )
+                    }
+                  }
+              }
+            )
+
+        case None =>
+          Future.successful(
+            Redirect(routes.SystemErrorController.onPageLoad())
+          )
+      }
+    }
+}
---
+++ test/controllers/RemoveAdditionalCorrespondenceNameYesNoControllerSpec.scala
@@ -0,0 +1,232 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import forms.RemoveAdditionalCorrespondenceNameYesNoFormProvider
+import models.{NormalMode, UserAnswers}
+import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.{CorrespondenceAdditionalNamePage, CorrespondenceDetailsSectionPage, RemoveAdditionalCorrespondenceNameYesNoPage}
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import repositories.SessionRepository
+import views.html.RemoveAdditionalCorrespondenceNameYesNoView
+
+import scala.concurrent.Future
+
+class RemoveAdditionalCorrespondenceNameYesNoControllerSpec extends SpecBase with MockitoSugar {
+
+  private val onwardRoute = Call("GET", "/foo")
+
+  private val formProvider = new RemoveAdditionalCorrespondenceNameYesNoFormProvider()
+  private val form = formProvider()
+
+  private lazy val removeAdditionalCorrespondenceNameYesNoRoute =
+    routes.RemoveAdditionalCorrespondenceNameYesNoController.onPageLoad(NormalMode).url
+
+  private val userAnswersWithAdditionalName =
+    emptyUserAnswers
+      .set(CorrespondenceDetailsSectionPage, userAnswersId)
+      .success
+      .value
+      .set(CorrespondenceAdditionalNamePage, "Name 2")
+      .success
+      .value
+
+  private val userAnswersWithoutAdditionalName =
+    emptyUserAnswers
+      .set(CorrespondenceDetailsSectionPage, userAnswersId)
+      .success
+      .value
+
+  "RemoveAdditionalCorrespondenceNameYesNo Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswersWithAdditionalName))
+          .build()
+
+      running(application) {
+        val request = FakeRequest(GET, removeAdditionalCorrespondenceNameYesNoRoute)
+
+        val result = route(application, request).value
+
+        val view =
+          application.injector.instanceOf[RemoveAdditionalCorrespondenceNameYesNoView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual
+          view(form, NormalMode, "Name 2")(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers =
+        userAnswersWithAdditionalName
+          .set(RemoveAdditionalCorrespondenceNameYesNoPage, true)
+          .success
+          .value
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswers))
+          .build()
+
+      running(application) {
+        val request = FakeRequest(GET, removeAdditionalCorrespondenceNameYesNoRoute)
+
+        val result = route(application, request).value
+
+        val view =
+          application.injector.instanceOf[RemoveAdditionalCorrespondenceNameYesNoView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual
+          view(form.fill(true), NormalMode, "Name 2")(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any[UserAnswers]))
+        .thenReturn(Future.successful(true))
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswersWithAdditionalName))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, removeAdditionalCorrespondenceNameYesNoRoute)
+            .withFormUrlEncodedBody("value" -> "true")
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswersWithAdditionalName))
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, removeAdditionalCorrespondenceNameYesNoRoute)
+            .withFormUrlEncodedBody("value" -> "")
+
+        val boundForm = form.bind(Map("value" -> ""))
+
+        val result = route(application, request).value
+
+        val view =
+          application.injector.instanceOf[RemoveAdditionalCorrespondenceNameYesNoView]
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual
+          view(boundForm, NormalMode, "Name 2")(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to System Error for a GET if no existing data is found" in {
+
+      val application =
+        applicationBuilder(userAnswers = None)
+          .build()
+
+      running(application) {
+        val request = FakeRequest(GET, removeAdditionalCorrespondenceNameYesNoRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual
+          routes.SystemErrorController.onPageLoad().url
+      }
+    }
+
+    "must redirect to System Error for a POST if no existing data is found" in {
+
+      val application =
+        applicationBuilder(userAnswers = None)
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, removeAdditionalCorrespondenceNameYesNoRoute)
+            .withFormUrlEncodedBody("value" -> "true")
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual
+          routes.SystemErrorController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery on GET when CorrespondenceAdditionalNamePage is missing" in {
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswersWithoutAdditionalName))
+          .build()
+
+      running(application) {
+        val request = FakeRequest(GET, removeAdditionalCorrespondenceNameYesNoRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual
+          routes.SystemErrorController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery on POST when CorrespondenceAdditionalNamePage is missing" in {
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswersWithoutAdditionalName))
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, removeAdditionalCorrespondenceNameYesNoRoute)
+            .withFormUrlEncodedBody("value" -> "true")
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual
+          routes.SystemErrorController.onPageLoad().url
+      }
+    }
+  }
+}
