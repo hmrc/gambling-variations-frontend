b2e2fbb4e3173ab8f4f0670c0794ee90611df089
lucafrancesc-hmrc <300928017+lucafrancesc-hmrc@users.noreply.github.com>
2026-07-23 16:44:19 +0100

    [DTR-7170]: Add tests for OtherTradeClassView

---
+++ test/views/OtherTradeClassViewSpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import base.SpecBase
+import forms.OtherTradeClassFormProvider
+import models.NormalMode
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.scalatest.matchers.must.Matchers.*
+import play.api.i18n.Messages
+import play.api.test.FakeRequest
+import views.html.OtherTradeClassView
+
+class OtherTradeClassViewSpec extends SpecBase {
+
+  trait Setup {
+    private val app = applicationBuilder().build()
+
+    private val view = app.injector.instanceOf[OtherTradeClassView]
+
+    implicit private val request: play.api.mvc.Request[?] = FakeRequest()
+
+    implicit val messages: Messages =
+      app.injector.instanceOf[play.api.i18n.MessagesApi].preferred(request)
+
+    private val formProvider = new OtherTradeClassFormProvider()
+    private val form = formProvider()
+
+    private val html = view(form, NormalMode)(request, messages)
+
+    val doc: Document = Jsoup.parse(html.body)
+
+  }
+
+  "OtherTradeClassView" - {
+
+    "must render page correctly" in new Setup {
+
+      doc.title must include(messages("otherTradeClass.title"))
+
+      doc.select("span").select(".govuk-caption-l").text() must include(messages("changeRegistrationDetails.caption"))
+
+      doc.select("#with-hint-hint").select(".govuk-hint").text() must include(messages("otherTradeClass.caption"))
+      doc.select("h1").select(".govuk-heading-l").text()         must include(messages("otherTradeClass.heading"))
+
+      doc
+        .select("#otherTradeClass-info")
+        .select(".govuk-hint")
+        .select(".govuk-character-count__message")
+        .text() must include(messages("otherTradeClass.textarea.label"))
+
+      doc.select("button.govuk-button").text must include(messages("site.continue"))
+
+    }
+
+  }
+}
