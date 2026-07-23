3fa8323e1708a0e5e2a2daff7db401246f446936
lucafrancesc-hmrc <300928017+lucafrancesc-hmrc@users.noreply.github.com>
2026-07-23 14:25:03 +0100

    [DTR-7170]: Add tests for CorrespondenceAdditionalNameView

---
+++ test/views/CorrespondenceAdditionalNameViewSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import base.SpecBase
+import forms.CorrespondenceAdditionalNameFormProvider
+import models.NormalMode
+import org.jsoup.Jsoup
+import org.scalatest.matchers.must.Matchers.*
+import play.api.i18n.Messages
+import play.api.test.FakeRequest
+import views.html.CorrespondenceAdditionalNameView
+
+class CorrespondenceAdditionalNameViewSpec extends SpecBase {
+
+  trait Setup {
+    val app = applicationBuilder().build()
+
+    val view = app.injector.instanceOf[CorrespondenceAdditionalNameView]
+
+    implicit val request: play.api.mvc.Request[?] = FakeRequest()
+
+    implicit val messages: Messages =
+      app.injector.instanceOf[play.api.i18n.MessagesApi].preferred(request)
+
+    val formProvider = new CorrespondenceAdditionalNameFormProvider()
+    val form = formProvider()
+
+    val html = view(form, NormalMode)(request, messages)
+
+    val doc = Jsoup.parse(html.body)
+
+  }
+
+  "CorrespondenceAdditionalNameView" - {
+
+    "must render page correctly" in new Setup {
+
+      doc.title must include(messages("correspondenceAdditionalName.title"))
+
+      doc.body().select(".govuk-caption-l").text() must include(messages("changeRegistrationDetails.caption"))
+
+      doc
+        .select(".govuk-label-wrapper")
+        .select(".govuk-label--l")
+        .text mustBe messages("correspondenceAdditionalName.heading")
+
+      doc.select("button.govuk-button").text must include(messages("site.continue"))
+
+    }
+
+  }
+}
