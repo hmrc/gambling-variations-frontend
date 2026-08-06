bbd387eb2fe3e10d78e1cf08ff4d853e157b79c2
AnotherMattWright <270914550+AnotherMattWright@users.noreply.github.com>
2026-08-06 14:20:57 +0100

    progress on onPageLoad and view

---
+++ app/controllers/CheckBusinessAddressController.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions.{AuthorisedAction, BusinessAddressDataRequiredAction, DataRetrievalAction}
+import pages.*
+import utils.FlagsUtil.checkFlag
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.BusinessAddressView
+
+import javax.inject.Inject
+
+class CheckBusinessAddressController @Inject() (
+                                              override val messagesApi: MessagesApi,
+                                              val controllerComponents: MessagesControllerComponents,
+                                              authorise: AuthorisedAction,
+                                              getData: DataRetrievalAction,
+                                              requireData: BusinessAddressDataRequiredAction,
+                                              view: BusinessAddressView
+                                            ) extends FrontendBaseController
+  with I18nSupport {
+
+  def onPageLoad: Action[AnyContent] = (authorise andThen getData andThen requireData) { implicit request =>
+    val ua = request.userAnswers
+    val showChangeMessage: Boolean = checkFlag(ua, BusinessAddressChangesPage, BusinessAddressSubmittedPage)
+    ua.get(BusinessAddressSectionPage) match {
+      case Some(_) => Ok(view(showChangeMessage))
+      case None => Redirect(routes.SystemErrorController.onPageLoad())
+    }
+  }
+}
