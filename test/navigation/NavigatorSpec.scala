81a71aab8d18f3296d13d8c8ee4bdd4e40d6eb0f
Javed <131012491+javed-iqbal1@users.noreply.github.com>
2026-08-07 11:06:42 +0100

    DTR-7373 add unit tests for navigator for add fax number for partner

--- test/navigation/NavigatorSpec.scala
+++ test/navigation/NavigatorSpec.scala
@@ -19,6 +19,7 @@ import base.SpecBase
 import controllers.routes
 import models.*
 import pages.*
+import pages.partner.PartnerAddFaxNumberYesNoPage
 
 class NavigatorSpec extends SpecBase {
 
@@ -692,6 +693,34 @@ class NavigatorSpec extends SpecBase {
 
         navigator.nextPage(CorrespondenceUKAddrScreenerPage, NormalMode, answers) mustBe
           routes.CheckCorrespondenceDetailsController.onPageLoad()
+      }
+
+
+      "should route PartnerAddFaxNumberYesNoPage to PartnerAddFaxNumberYesNoController when answer is true" in {
+        val answers =
+          emptyAnswers
+            .set(PartnerAddFaxNumberYesNoPage, true)
+            .success
+            .value
+
+        navigator.nextPage(PartnerAddFaxNumberYesNoPage, NormalMode, answers) mustBe
+          controllers.partner.routes.PartnerAddFaxNumberYesNoController.onPageLoad() // update later
+      }
+
+      "should route PartnerAddFaxNumberYesNoPage to PartnerAddFaxNumberYesNoController when answer is false" in {
+        val answers =
+          emptyAnswers
+            .set(PartnerAddFaxNumberYesNoPage, false)
+            .success
+            .value
+
+        navigator.nextPage(PartnerAddFaxNumberYesNoPage, NormalMode, answers) mustBe
+          controllers.partner.routes.PartnerAddFaxNumberYesNoController.onPageLoad() //update later
+      }
+
+      "should route PartnerAddFaxNumberYesNoPage to SystemError when unanswered" in {
+        navigator.nextPage(PartnerAddFaxNumberYesNoPage, NormalMode, emptyAnswers) mustBe
+          routes.SystemErrorController.onPageLoad()
       }
 
       "should route RemoveCorrespondenceDetailsYesNoPage to SystemError when unanswered" in {
