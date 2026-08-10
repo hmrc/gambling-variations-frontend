64cb222b55aaf95d1b3ebf786a0208ab2e5ec8bc
AnotherMattWright <270914550+AnotherMattWright@users.noreply.github.com>
2026-08-10 10:40:53 +0100

    additionaladdressinforow added

--- app/viewmodels/checkAnswers/businessaddress/AddressAdditionalInfoRow.scala
+++ app/viewmodels/checkAnswers/businessaddress/AddressAdditionalInfoRow.scala
@@ -1,5 +1,21 @@
 package viewmodels.checkAnswers.businessaddress
 
-class AddressAdditionalInfoRow {
-  
+import controllers.routes
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import viewmodels.govuk.all.{ActionItemViewModel, SummaryListRowViewModel, ValueViewModel}
+import viewmodels.implicits.*
+
+
+case class AddressAdditionalInfoRow(isAdded: Boolean)(implicit messages: Messages) {
+  def toRow: SummaryListRow = {
+    val yesNoValue = ValueViewModel(
+      if(isAdded) "site.yes" else "site.no"
+    )
+
+    SummaryListRowViewModel(
+      key = "checkBusinessAddress.question.addInfo",
+      value = yesNoValue,
+      actions = Seq(ActionItemViewModel("site.change", routes.PageNotFoundController.onPageLoad().url))
+    )
+  }
 }
