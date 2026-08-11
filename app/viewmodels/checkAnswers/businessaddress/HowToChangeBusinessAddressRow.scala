437dfdeec9fce73f5b9864a7ffa2ce01d92f5124
AnotherMattWright <270914550+AnotherMattWright@users.noreply.github.com>
2026-08-11 11:32:21 +0100

    created change address option list

---
+++ app/viewmodels/checkAnswers/businessaddress/HowToChangeBusinessAddressRow.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.checkAnswers.businessaddress
+
+import controllers.routes
+import models.{CheckMode, Mode, UserAnswers}
+import pages.CorrespondenceChangeAddrScreenerPage
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.{SummaryListRow, Value}
+import viewmodels.govuk.all.{ActionItemViewModel, SummaryListRowViewModel, ValueViewModel}
+import viewmodels.implicits.*
+
+case object HowToChangeBusinessAddressRow {
+  def from(ua: UserAnswers)(implicit messages: Messages): SummaryListRow = {
+    SummaryListRowViewModel(
+      key = "checkBusinessAddress.question.howToChange",
+      value = ValueViewModel(
+        ua.get(CorrespondenceChangeAddrScreenerPage) match {
+          case Some(howToChange) => howToChange match {
+          
+        }
+          case None => "site.notProvided"
+        }),
+      actions = Seq(ActionItemViewModel("site.change", "#"))
+    )
+  }
+}
