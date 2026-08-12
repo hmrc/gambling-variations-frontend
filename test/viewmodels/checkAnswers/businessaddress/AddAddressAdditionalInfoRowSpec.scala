48c2b000e19e5f2509064899136e02be8988d432
AnotherMattWright <270914550+AnotherMattWright@users.noreply.github.com>
2026-08-12 14:49:19 +0100

    updated yesNo row specs

--- test/viewmodels/checkAnswers/businessaddress/AddAddressAdditionalInfoRowSpec.scala
+++ test/viewmodels/checkAnswers/businessaddress/AddAddressAdditionalInfoRowSpec.scala
@@ -1,3 +1,51 @@
 package viewmodels.checkAnswers.businessaddress
 
-class AddAddressAdditionalInfoRowSpec {}
+import base.SpecBase
+import models.BusinessAddressChangeAddrOption.*
+import models.{Address, UserAnswers}
+import pages.*
+import play.api.Application
+import play.api.i18n.Messages
+import play.api.test.FakeRequest
+import uk.gov.hmrc.govukfrontend.views.Aliases.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+
+class AddAddressAdditionalInfoRowSpec extends SpecBase {
+
+  trait Setup {
+    val app: Application = applicationBuilder().build()
+    implicit val request: play.api.mvc.Request[?] = FakeRequest()
+    implicit val messages: Messages =
+      app.injector
+        .instanceOf[play.api.i18n.MessagesApi]
+        .preferred(request)
+
+  }
+
+  "AddAddressAdditionalInfoRow" - {
+
+    "must render summary list row with correct data" in new Setup {
+      private val addressUa: Address = Address(
+        "address1",
+        Some("address2"),
+        Some("address3"),
+        Some("address4"),
+        Some("postcode"),
+        Some("country"))
+
+
+      private val ua = emptyUserAnswers.
+        set(BusinessAddressUkPage, addressUa)
+        .success
+        .value
+        .set(AddBusinessAddressAdditionalInformationPage, true)
+        .success
+        .value
+
+      private val result: SummaryListRow = AddAddressAdditionalInfoRow.from(ua)
+
+      result.key.content mustEqual Text(messages("checkBusinessAddress.question.addInfo"))
+      result.value.content mustEqual Text(messages("site.yes"))
+    }
+  }
+}
