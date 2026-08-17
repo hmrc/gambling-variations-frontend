2c2c1fc1946b6fb43d223300c1ae3e028924f220
benmasters0 <117096227+benmasters0@users.noreply.github.com>
2026-08-17 10:20:23 +0100

    summary fix

--- app/viewmodels/checkAnswers/BusinessUKAddrScreenerSummary.scala
+++ app/viewmodels/checkAnswers/BusinessUKAddrScreenerSummary.scala
@@ -35,7 +35,7 @@ object BusinessUKAddrScreenerSummary {
         key   = "businessUKAddrScreener.checkYourAnswersLabel",
         value = ValueViewModel(value),
         actions = Seq(
-          ActionItemViewModel("site.change", routes.BusinessUKAddrScreenerController.onPageLoad(CheckMode).url)
+          ActionItemViewModel("site.change", routes.BusinessUKAddrScreenerController.onPageLoad().url)
             .withVisuallyHiddenText(messages("businessUKAddrScreener.change.hidden"))
         )
       )
