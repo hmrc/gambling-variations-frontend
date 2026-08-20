d0f80f060cba6db0061d1c45aadcea2e57c584cc
lucafrancesc-hmrc <300928017+lucafrancesc-hmrc@users.noreply.github.com>
2026-08-20 09:30:24 +0100

    [DTR-7895] Fixes after merge: Navigator

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -140,8 +140,6 @@ class Navigator @Inject() () {
       _ => controllers.partner.routes.PartnerEmailAddressController.onPageLoad()
     case PartnerDetailsContactNumberPage(index) =>
       _ => controllers.partner.routes.PartnerContactDetailsController.onPageLoad()
-    case PartnerDetailsRemoveEmailAddressYesNoPage(index) =>
-      userAnswers => navigatePartnerRemoveEmailYesNoPage(userAnswers, index)
     case PartnerDetailsNinoPage(index) =>
       userAnswers => navigatePartnerNinoYesNoPage(index)(userAnswers)
 
