98bd6448ee5ace7e9b8962cce9f108a94caf1c0c
Luca Francesco <300928017+lucafrancesc-hmrc@users.noreply.github.com>
2026-08-20 15:46:17 +0100

    [DTR-7898] Screener for National Insurance number - Screen build (#201)

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -141,7 +141,9 @@ class Navigator @Inject() () {
     case PartnerDetailsContactNumberPage(index) =>
       _ => controllers.partner.routes.PartnerContactDetailsController.onPageLoad()
     case PartnerDetailsNinoPage(index) =>
-      userAnswers => navigatePartnerNinoYesNoPage(index)(userAnswers)
+      userAnswers => navigatePartnerRemoveNinoYesNoPage(index)(userAnswers)
+    case PartnerDetailsAddNationalInsuranceNumberYesNoPage(index) =>
+      userAnswers => navigatePartnerAddNinoYesNoPage(index)(userAnswers)
 
     case PartnerDetailsTradingNamePage(index) =>
       _ => controllers.partner.routes.PartnerTradingNameController.onPageLoad() // change it
@@ -407,10 +409,23 @@ class Navigator @Inject() () {
       .map(_ => controllers.partner.routes.PartnerAddFaxNumberYesNoController.onPageLoad())
       .getOrElse(routes.SystemErrorController.onPageLoad())
 
-  private def navigatePartnerNinoYesNoPage(index: Int)(answers: UserAnswers): Call =
+  private def navigatePartnerRemoveNinoYesNoPage(index: Int)(answers: UserAnswers): Call =
     answers
       .get(PartnerDetailsRemoveNationalInsuranceNumberYesNoPage(index))
       .map(_ => controllers.partner.routes.PartnerDetailsRemoveNationalInsuranceNumberYesNoController.onPageLoad())
+      .getOrElse(routes.SystemErrorController.onPageLoad())
+
+  private def navigatePartnerAddNinoYesNoPage(index: Int)(answers: UserAnswers): Call =
+    answers
+      .get(PartnerDetailsAddNationalInsuranceNumberYesNoPage(index))
+      .map {
+        case false =>
+          // Should go to Add Nino
+          controllers.partner.routes.PartnerDetailsAddNationalInsuranceNumberYesNoController.onPageLoad()
+        case true =>
+          // Should go to Trading name
+          controllers.partner.routes.PartnerDetailsAddNationalInsuranceNumberYesNoController.onPageLoad()
+      }
       .getOrElse(routes.SystemErrorController.onPageLoad())
 
   private def navigatePartnerAddEmailAddressYesNoPage(index: Int)(answers: UserAnswers): Call =
