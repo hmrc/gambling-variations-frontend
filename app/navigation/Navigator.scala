ae14adbc5e341ec6056315c32682d2f1bc70c3ba
Javed Iqbal <131012491+javed-iqbal1@users.noreply.github.com>
2026-08-20 15:23:58 +0100

    DTR-7623 Add/change Partner trading name (#199)
    DTR-7623 Add/change Partner trading name (#199)

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -22,7 +22,7 @@ import models.BusinessType.Soleproprietor
 import models.CorrespondenceChangeAddrOption.*
 import pages.*
 import pages.partner.*
-import pages.partnerdetails.{PartnerDetailsContactNumberPage, PartnerDetailsNinoPage}
+import pages.partnerdetails.{PartnerDetailsContactNumberPage, PartnerDetailsNinoPage, PartnerDetailsTradingNamePage}
 import play.api.mvc.Call
 
 import javax.inject.{Inject, Singleton}
@@ -143,6 +143,8 @@ class Navigator @Inject() () {
     case PartnerDetailsNinoPage(index) =>
       userAnswers => navigatePartnerNinoYesNoPage(index)(userAnswers)
 
+    case PartnerDetailsTradingNamePage(index) =>
+      _ => controllers.partner.routes.PartnerTradingNameController.onPageLoad() // change it
     case _ =>
       _ => routes.IndexController.onPageLoad()
   }
