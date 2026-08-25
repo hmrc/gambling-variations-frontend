34b5737fa94533ce35a8afc1ef37788489d0cb18
Milosz Jakubanis <301772132+milosz-jakubanis@users.noreply.github.com>
2026-08-25 15:07:56 +0100

    feat: Add tests for partnerdetails pages DTR-7939

---
+++ test/pages/partnerdetails/PartnerDetailsForeignCorporateRefPageSpec.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pages.partnerdetails
+
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.{JsPath, Json}
+
+
+class PartnerDetailsForeignCorporateRefPageSpec extends PlaySpec {
+
+  val Index = 0
+
+  "PartnerDetailsForeignCorporateRefPage" must {
+
+    "have the correct path" in {
+      PartnerDetailsForeignCorporateRefPage(Index).path mustEqual (JsPath \ "partners" \ Index \ "partnerDetailsForeignCorporateRef")
+    }
+
+    "have the correct toString value" in {
+
+      PartnerDetailsForeignCorporateRefPage(Index).toString mustEqual "partnerDetailsForeignCorporateRef"
+    }
+
+    "be able to read and write PartnerDetailsForeignCorporateRefPage values with correct index" in {
+
+      val value1 = "Value1"
+      val value2 = "Value2"
+
+      val json = Json.obj(
+        "partners" -> Json.arr(
+          Json.obj(
+            PartnerDetailsForeignCorporateRefPage(Index).toString -> Json.toJson(value1)
+          ),
+          Json.obj(
+            PartnerDetailsForeignCorporateRefPage(Index + 1).toString -> Json.toJson(value2)
+          )
+        )
+      )
+
+      PartnerDetailsForeignCorporateRefPage(Index).path
+        .asSingleJson(json)
+        .validate[String]
+        .get mustEqual value1
+
+      PartnerDetailsForeignCorporateRefPage(Index + 1).path
+        .asSingleJson(json)
+        .validate[String]
+        .get mustEqual value2
+    }
+
+  }
+}
---
+++ test/pages/partnerdetails/PartnerDetailsSoleProprietorPageSpec.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pages.partnerdetails
+
+import models.SoleProprietorName
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.{JsPath, Json}
+
+class PartnerDetailsSoleProprietorPageSpec extends PlaySpec {
+
+  val Index = 0
+
+  "PartnerDetailsSoleProprietorPage" must {
+
+    "have the correct path" in {
+      PartnerDetailsSoleProprietorPage(Index).path mustEqual (JsPath \ "partners" \ Index \ "partnerDetailsSoleProprietor")
+    }
+
+    "have the correct toString value" in {
+
+      PartnerDetailsSoleProprietorPage(Index).toString mustEqual "partnerDetailsSoleProprietor"
+    }
+
+    "be able to read and write PartnerDetailsSoleProprietorPage values with correct index" in {
+
+      val value1 = SoleProprietorName(
+        title      = "111",
+        firstName  = "111",
+        middleName = Some("111"),
+        lastName   = "111"
+      )
+
+      val value2 = SoleProprietorName(
+        title      = "222",
+        firstName  = "222",
+        middleName = Some("222"),
+        lastName   = "222"
+      )
+
+      val json = Json.obj(
+        "partners" -> Json.arr(
+          Json.obj(
+            PartnerDetailsSoleProprietorPage(Index).toString -> Json.toJson(value1)
+          ),
+          Json.obj(
+            PartnerDetailsSoleProprietorPage(Index + 1).toString -> Json.toJson(value2)
+          )
+        )
+      )
+
+      PartnerDetailsSoleProprietorPage(Index).path
+        .asSingleJson(json)
+        .validate[SoleProprietorName]
+        .get mustEqual value1
+
+      PartnerDetailsSoleProprietorPage(Index + 1).path
+        .asSingleJson(json)
+        .validate[SoleProprietorName]
+        .get mustEqual value2
+    }
+
+  }
+}
