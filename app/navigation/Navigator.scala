a09a71b0932a79814385981d3aa626e131e2a3a2
lucafrancesc-hmrc <300928017+lucafrancesc-hmrc@users.noreply.github.com>
2026-08-26 16:02:22 +0100

    [DTR-7904] Update navigator

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -161,7 +161,7 @@ class Navigator @Inject() () {
     case PartnerDetailsTradingNamePage(index) =>
       _ => controllers.partner.routes.PartnerTradingNameController.onPageLoad() // change it
     case PartnerDetailsAddTradingNameYesNoPage(index) =>
-      _ => controllers.partner.routes.PartnerDetailsAddTradingNameYesNoController.onPageLoad() // change it
+      userAnswers => navigatePartnerAddTradingNameYesNoPage(index)(userAnswers)
 
     case _ =>
       _ => routes.IndexController.onPageLoad()
@@ -445,6 +445,19 @@ class Navigator @Inject() () {
         case true =>
           // Should go to Trading name
           controllers.partner.routes.PartnerDetailsAddNationalInsuranceNumberYesNoController.onPageLoad()
+      }
+      .getOrElse(routes.SystemErrorController.onPageLoad())
+
+  private def navigatePartnerAddTradingNameYesNoPage(index: Int)(answers: UserAnswers): Call =
+    answers
+      .get(PartnerDetailsAddTradingNameYesNoPage(index))
+      .map {
+        case false =>
+          // Should go to Add/change trading name
+          controllers.partner.routes.PartnerDetailsAddTradingNameYesNoController.onPageLoad()
+        case true =>
+          // Should go to Is the partner's business incorporated in the UK? or PT-UTR - UTR Taxpayer Reference
+          controllers.partner.routes.PartnerDetailsAddTradingNameYesNoController.onPageLoad()
       }
       .getOrElse(routes.SystemErrorController.onPageLoad())
 
