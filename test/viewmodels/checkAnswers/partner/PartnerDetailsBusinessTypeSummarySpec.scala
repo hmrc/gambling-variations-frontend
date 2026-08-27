a74a050d2f005105fd887e3cbfd1ed679b62e046
lucafrancesc-hmrc <300928017+lucafrancesc-hmrc@users.noreply.github.com>
2026-08-27 11:25:03 +0100

    [DTR-7558] add tests for PartnerDetailsBusinessTypeSummary

---
+++ test/viewmodels/checkAnswers/partner/PartnerDetailsBusinessTypeSummarySpec.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.checkAnswers.partner
+
+import base.SpecBase
+import models.PartnerDetailsBusinessType.CorporateBody
+import pages.partner.PartnerDetailsBusinessTypePage
+import play.api.Application
+import play.api.i18n.Messages
+import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
+import viewmodels.govuk.summarylist.*
+import viewmodels.implicits.*
+
+class PartnerDetailsBusinessTypeSummarySpec extends SpecBase {
+
+  lazy val app: Application = applicationBuilder().build()
+  implicit val messages: Messages = this.messages(app)
+
+  private val index: Int = 0
+
+  "PartnerDetailsBusinessTypeSummary" - {
+
+    "must return None when the question has not been answered" in {
+      PartnerDetailsBusinessTypeSummary.row(emptyUserAnswers) mustBe None
+    }
+
+    "must return the correct row when an answer exists" in {
+      val answers =
+        emptyUserAnswers
+          .set(PartnerDetailsBusinessTypePage(index), CorporateBody)
+          .success
+          .value
+
+      val expectedValue = ValueViewModel(
+        HtmlContent(
+          HtmlFormat.escape(messages(s"partnerDetailsBusinessType.$CorporateBody"))
+        )
+      )
+
+      PartnerDetailsBusinessTypeSummary.row(answers).value mustBe
+        SummaryListRowViewModel(
+          key   = "partnerDetailsBusinessType.checkYourAnswersLabel",
+          value = expectedValue,
+          actions = Seq(
+            ActionItemViewModel(
+              "site.change",
+              controllers.partner.routes.PartnerDetailsBusinessTypeController.onPageLoad().url
+            ).withVisuallyHiddenText(
+              messages("partnerDetailsBusinessType.change.hidden")
+            )
+          )
+        )
+    }
+  }
+}
