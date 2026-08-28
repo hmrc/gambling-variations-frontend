4658b6135496216174830bc9ff75004b3a7889b7
Milosz Jakubanis <301772132+milosz-jakubanis@users.noreply.github.com>
2026-08-28 14:06:05 +0100

    feat: Wip DTR-7939

--- app/controllers/ChangePartnerDetailsBusinessNameController.scala
+++ app/controllers/ChangePartnerDetailsBusinessNameController.scala
@@ -19,11 +19,13 @@ package controllers
 import controllers.actions.*
 import forms.{ChangeBusinessNameFormProvider, SoleProprietorNameFormProvider}
 import models.BusinessType.Soleproprietor
-import models.{BusinessType, Mode}
+import models.{BusinessType, Mode, UserAnswers}
 import navigation.Navigator
 import pages.*
 import pages.partnerdetails.{PartnerDetailsBusinessNamePage, PartnerDetailsBusinessTypePage, PartnerDetailsSoleProprietorPage}
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.json.Reads.JsArrayReads
+import play.api.libs.json.{JsArray, JsPath, Reads}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
@@ -49,25 +51,21 @@ class ChangePartnerDetailsBusinessNameController @Inject() (
     extends FrontendBaseController
     with I18nSupport {
 
-  /*TODO: Important! This controller will be adding a new partner, it will have very minimal
-     information at this stage and till the end before submitting this information it won't have businessPartnerNumber.
-     Lack of it implies data is ONLY in the cache and has not been submitted yet.
-   */
-  private val Index: Int = 0
-
   def onPageLoad(businessType: BusinessType, mode: Mode): Action[AnyContent] = {
     (authorise andThen getData andThen requireData) { implicit request =>
-      (request.userAnswers.get(PartnerDetailsBusinessTypePage(Index)) map {
+      val newIndex = getPartnersSize(request.userAnswers)
+
+      (request.userAnswers.get(PartnerDetailsBusinessTypePage(newIndex)) map {
         case Soleproprietor =>
           val form = request.userAnswers
-            .get(PartnerDetailsSoleProprietorPage(Index))
+            .get(PartnerDetailsSoleProprietorPage(newIndex))
             .fold(soleProprietorFormProvider())(soleProp => soleProprietorFormProvider().fill(soleProp))
           Ok(changeSoleProprietorView(form, mode))
         case businessType =>
           val headingKey = BusinessTypeKeyBuilder.headingKeyFor(businessType)
           val titleKey = BusinessTypeKeyBuilder.titleKeyFor(businessType)
           val form = request.userAnswers
-            .get(PartnerDetailsBusinessNamePage(Index))
+            .get(PartnerDetailsBusinessNamePage(newIndex))
             .fold(businessNameFormProvider(businessType))(businessName => businessNameFormProvider(businessType).fill(businessName))
           Ok(changeBusinessNameView(form, mode, businessType, headingKey, titleKey))
       }).getOrElse(Redirect(routes.SystemErrorController.onPageLoad()))
@@ -76,7 +74,9 @@ class ChangePartnerDetailsBusinessNameController @Inject() (
 
   def onSubmit(businessType: BusinessType, mode: Mode): Action[AnyContent] = {
     (authorise andThen getData andThen requireData).async { implicit request =>
-      request.userAnswers.get(PartnerDetailsBusinessTypePage(Index)) map {
+      val newIndex = getPartnersSize(request.userAnswers)
+
+      request.userAnswers.get(PartnerDetailsBusinessTypePage(newIndex)) map {
         case Soleproprietor =>
           soleProprietorFormProvider()
             .bindFromRequest()
@@ -86,9 +86,9 @@ class ChangePartnerDetailsBusinessNameController @Inject() (
               },
               value =>
                 for {
-                  updatedAnswers <- Future.fromTry(request.userAnswers.set(PartnerDetailsSoleProprietorPage(Index), value))
+                  updatedAnswers <- Future.fromTry(request.userAnswers.set(PartnerDetailsSoleProprietorPage(newIndex), value))
                   _              <- sessionRepository.set(updatedAnswers)
-                } yield Redirect(navigator.nextPage(PartnerDetailsSoleProprietorPage(Index), mode, updatedAnswers))
+                } yield Redirect(navigator.nextPage(PartnerDetailsSoleProprietorPage(newIndex), mode, updatedAnswers))
             )
         case businessType =>
           val headingKey = BusinessTypeKeyBuilder.headingKeyFor(businessType)
@@ -100,12 +100,21 @@ class ChangePartnerDetailsBusinessNameController @Inject() (
               formWithErrors => Future.successful(BadRequest(changeBusinessNameView(formWithErrors, mode, businessType, headingKey, titleKey))),
               value =>
                 for {
-                  updatedAnswers <- Future.fromTry(request.userAnswers.set(PartnerDetailsBusinessNamePage(Index), value))
+                  updatedAnswers <- Future.fromTry(request.userAnswers.set(PartnerDetailsBusinessNamePage(newIndex), value))
                   _              <- sessionRepository.set(updatedAnswers)
-                } yield Redirect(navigator.nextPage(PartnerDetailsBusinessNamePage(Index), mode, updatedAnswers))
+                } yield Redirect(navigator.nextPage(PartnerDetailsBusinessNamePage(newIndex), mode, updatedAnswers))
             )
       } getOrElse Future.successful(Redirect(routes.SystemErrorController.onPageLoad()))
     }
   }
+
+  /*TODO: Important! This controller will be adding a new partner, it will have very minimal
+     information at this stage and till the end before submitting this information it won't have businessPartnerNumber.
+     Lack of it implies data is ONLY in the cache and has not been submitted yet.
+   */
+  private def getPartnersSize(userAnswers: UserAnswers) = (userAnswers.data \ "partners")
+    .validate[JsArray]
+    .asOpt
+    .fold(0)(e => if e.value.isEmpty then 0 else e.value.size - 1)
 
 }
