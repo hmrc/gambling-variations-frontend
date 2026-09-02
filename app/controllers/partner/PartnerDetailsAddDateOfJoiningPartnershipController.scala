7da698533dacaaaac88450b191d317ba60fd8be3
Milosz Jakubanis <301772132+milosz-jakubanis@users.noreply.github.com>
2026-09-02 11:30:55 +0100

    feat: Messages, validation, html DTR-8080

---
+++ app/controllers/partner/PartnerDetailsAddDateOfJoiningPartnershipController.scala
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.partner
+
+import controllers.actions.*
+import controllers.routes
+import forms.partner.PartnerDetailsAddDateOfJoiningPartnershipFormProvider
+import models.{Mode, UserAnswers}
+import navigation.Navigator
+import pages.partnerdetails.{PartnerDetailsDateOfIncorporation, PartnerDetailsDateOfJoiningPage}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.json.JsArray
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.partner.PartnerDetailsAddDateOfJoiningPartnershipView
+import utils.DateTimeFormats.dateTimeFormat
+import play.api.i18n.Lang
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class PartnerDetailsAddDateOfJoiningPartnershipController @Inject() (
+  override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
+  navigator: Navigator,
+  authorise: AuthorisedAction,
+  getData: DataRetrievalAction,
+  requireData: PartnerDetailsDataRequiredAction,
+  formProvider: PartnerDetailsAddDateOfJoiningPartnershipFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  view: PartnerDetailsAddDateOfJoiningPartnershipView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (authorise andThen getData andThen requireData) { implicit request =>
+
+    val newIndex = getPartnersSize(request.userAnswers)
+
+    request.userAnswers.get(PartnerDetailsDateOfJoiningPage(newIndex)) match {
+      case None =>
+        Redirect(routes.SystemErrorController.onPageLoad())
+      case Some(dateOfJoining) =>
+        val form = request.userAnswers
+          .get(PartnerDetailsDateOfIncorporation(newIndex))
+          .fold(formProvider(dateOfJoining))(formProvider(dateOfJoining).fill)
+
+        val formatter = dateTimeFormat()(Lang("en"))
+        val dateOfJoiningFormatted = dateOfJoining.format(formatter)
+        val twoWeeksLaterFormatted = dateOfJoining.plusDays(14).format(formatter)
+
+        Ok(view(form, mode, dateOfJoiningFormatted, twoWeeksLaterFormatted)) // TODO
+    }
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (authorise andThen getData andThen requireData).async { implicit request =>
+
+    val newIndex = getPartnersSize(request.userAnswers)
+
+    request.userAnswers.get(PartnerDetailsDateOfJoiningPage(newIndex)) match {
+      case None =>
+        Future.successful(Redirect(routes.SystemErrorController.onPageLoad()))
+      case Some(dateOfJoining) =>
+        request.userAnswers
+          .get(PartnerDetailsDateOfIncorporation(newIndex))
+          .fold(formProvider(dateOfJoining))(formProvider(dateOfJoining).fill)
+          .bindFromRequest()
+          .fold(
+            formWithErrors =>
+              val formatter = dateTimeFormat()(Lang("en"))
+              val dateOfJoiningFormatted = dateOfJoining.format(formatter)
+              val twoWeeksLaterFormatted = dateOfJoining.plusDays(14).format(formatter)
+              Future.successful(BadRequest(view(formWithErrors, mode, dateOfJoiningFormatted, twoWeeksLaterFormatted)))
+            , // TODO
+            value =>
+              for {
+                updatedAnswers <- Future.fromTry(request.userAnswers.set(PartnerDetailsDateOfIncorporation(newIndex), value))
+                _              <- sessionRepository.set(updatedAnswers)
+              } yield Redirect(navigator.nextPage(PartnerDetailsDateOfIncorporation(newIndex), mode, updatedAnswers))
+          )
+    }
+  }
+
+  // TODO TODO TODO I WONT NEED IT HERE
+  private def getPartnersSize(userAnswers: UserAnswers): Int = (userAnswers.data \ "partners")
+    .validate[JsArray]
+    .asOpt
+    .fold(0)(e => if e.value.isEmpty then 0 else e.value.size - 1)
+}
