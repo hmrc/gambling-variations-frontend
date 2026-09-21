d64407508eb320e7668a7d1eb7f22d2c2322262b
derekspalmer <35488891+derekspalmer@users.noreply.github.com>
2026-09-21 12:01:37 +0100

    DTR-8213: rework VAT RegYesNoController

--- test/controllers/partnerdetails/PartnerDetailsVatRegistrationNumberYesNoControllerSpec.scala
+++ test/controllers/partnerdetails/PartnerDetailsVatRegistrationNumberYesNoControllerSpec.scala
@@ -15,185 +15,206 @@
  */
 
 package controllers.partnerdetails
-//
-//import base.SpecBase
-//import forms.partnerdetails.VatRegistrationNumberYesNoFormProvider
-//import models.NormalMode
-//import navigation.{FakeNavigator, Navigator}
-//import org.mockito.ArgumentMatchers.any
-//import org.mockito.Mockito.when
-//import org.scalatestplus.mockito.MockitoSugar
-//import pages.partnerdetails.{PartnerDetailsAddPartnerCompletedPage, PartnerDetailsMgdRegNumberPage, PartnerDetailsVatRegistrationNumberYesNoPage}
-//import play.api.inject.bind
-//import play.api.mvc.Call
-//import play.api.test.FakeRequest
-//import play.api.test.Helpers.*
-//import repositories.SessionRepository
-//import views.html.partner.VatRegistrationNumberYesNoView
-//
-//import scala.concurrent.Future
-//
-//class PartnerDetailsVatRegistrationNumberYesNoControllerSpec extends SpecBase with MockitoSugar {
-//
-//  val businessNumber: String = "12345"
-//  def onwardRoute = Call("GET", "/foo")
-//
-//  val formProvider = new VatRegistrationNumberYesNoFormProvider()
-//  val form = formProvider()
-//
-//  lazy val vatRegistrationNumberYesNoRoute =
-//    controllers.partnerdetails.routes.VatRegistrationNumberYesNoController.onPageLoad().url
-//
-//  private val userAnswersWithoutVatAnswer =
-//    emptyUserAnswers
-//      .set(PartnerDetailsAddPartnerCompletedPage(businessNumber), false)
-//      .success
-//      .value
-//      .set(PartnerDetailsMgdRegNumberPage(businessNumber), "123456789")
-//      .success
-//      .value
-//
-//  private val userAnswersWithVatAnswer =
-//    userAnswersWithoutVatAnswer
-//      .set(PartnerDetailsVatRegistrationNumberYesNoPage(businessNumber), true)
-//      .success
-//      .value
-//
-//  "VatRegistrationNumberYesNoController Controller" - {
-//
-//    "must return OK and the correct view for a GET" in {
-//
-//      val application =
-//        applicationBuilder(userAnswers = Some(userAnswersWithoutVatAnswer)).build()
-//
-//      running(application) {
-//        val request =
-//          FakeRequest(GET, vatRegistrationNumberYesNoRoute)
-//
-//        val result =
-//          route(application, request).value
-//
-//        val view =
-//          application.injector.instanceOf[VatRegistrationNumberYesNoView]
-//
-//        status(result) mustEqual OK
-//        contentAsString(result) mustEqual
-//          view(
-//            form,
-//            NormalMode
-//          )(request, messages(application)).toString
-//      }
-//    }
-//
-//    "must populate the view correctly on a GET when the question has previously been answered" in {
-//
-//      val application =
-//        applicationBuilder(userAnswers = Some(userAnswersWithVatAnswer)).build()
-//
-//      running(application) {
-//        val request =
-//          FakeRequest(GET, vatRegistrationNumberYesNoRoute)
-//
-//        val view =
-//          application.injector.instanceOf[VatRegistrationNumberYesNoView]
-//
-//        val result =
-//          route(application, request).value
-//
-//        status(result) mustEqual OK
-//        contentAsString(result) mustEqual
-//          view(
-//            form.fill(true),
-//            NormalMode
-//          )(request, messages(application)).toString
-//      }
-//    }
-//
-//    "must redirect to the next page when true is submitted" in {
-//
-//      val mockSessionRepository =
-//        mock[SessionRepository]
-//
-//      when(mockSessionRepository.set(any()))
-//        .thenReturn(Future.successful(true))
-//
-//      val application =
-//        applicationBuilder(userAnswers = Some(userAnswersWithoutVatAnswer))
-//          .overrides(
-//            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
-//            bind[SessionRepository].toInstance(mockSessionRepository)
-//          )
-//          .build()
-//
-//      running(application) {
-//        val request =
-//          FakeRequest(POST, vatRegistrationNumberYesNoRoute)
-//            .withFormUrlEncodedBody(("value", "true"))
-//
-//        val result =
-//          route(application, request).value
-//
-//        status(result) mustEqual SEE_OTHER
-//        redirectLocation(result).value mustEqual onwardRoute.url
-//      }
-//    }
-//
-//    "must redirect to the next page when false is submitted" in {
-//
-//      val mockSessionRepository =
-//        mock[SessionRepository]
-//
-//      when(mockSessionRepository.set(any()))
-//        .thenReturn(Future.successful(true))
-//
-//      val application =
-//        applicationBuilder(userAnswers = Some(userAnswersWithoutVatAnswer))
-//          .overrides(
-//            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
-//            bind[SessionRepository].toInstance(mockSessionRepository)
-//          )
-//          .build()
-//
-//      running(application) {
-//        val request =
-//          FakeRequest(POST, vatRegistrationNumberYesNoRoute)
-//            .withFormUrlEncodedBody(("value", "false"))
-//
-//        val result =
-//          route(application, request).value
-//
-//        status(result) mustEqual SEE_OTHER
-//        redirectLocation(result).value mustEqual onwardRoute.url
-//      }
-//    }
-//
-//    "must return a Bad Request and errors when invalid data is submitted" in {
-//
-//      val application =
-//        applicationBuilder(userAnswers = Some(userAnswersWithoutVatAnswer)).build()
-//
-//      running(application) {
-//        val request =
-//          FakeRequest(POST, vatRegistrationNumberYesNoRoute)
-//            .withFormUrlEncodedBody(("value", ""))
-//
-//        val boundForm =
-//          form.bind(Map("value" -> ""))
-//
-//        val view =
-//          application.injector.instanceOf[VatRegistrationNumberYesNoView]
-//
-//        val result =
-//          route(application, request).value
-//
-//        status(result) mustEqual BAD_REQUEST
-//        contentAsString(result) mustEqual
-//          view(
-//            boundForm,
-//            NormalMode
-//          )(request, messages(application)).toString
-//      }
-//    }
-//
-//  }
-//}
+
+import base.SpecBase
+import forms.partnerdetails.VatRegistrationNumberYesNoFormProvider
+import models.NormalMode
+import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{verify, when}
+import org.scalatestplus.mockito.MockitoSugar
+import pages.partnerdetails.{PartnerDetailsAddPartnerCompletedPage, PartnerDetailsMgdRegNumberPage, PartnerDetailsVatRegistrationNumberYesNoPage}
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import repositories.SessionRepository
+import views.html.partnerdetails.PartnerDetailsVatRegistrationNumberYesNoView
+
+import scala.concurrent.Future
+
+class PartnerDetailsVatRegistrationNumberYesNoControllerSpec extends SpecBase with MockitoSugar {
+
+  val index = 0
+  val businessNumber: String = "12345"
+  def onwardRoute = Call("GET", "/foo")
+
+  val formProvider = new VatRegistrationNumberYesNoFormProvider()
+  val form = formProvider()
+
+  lazy val vatRegistrationNumberYesNoRoute =
+    controllers.partnerdetails.routes.PartnerDetailsVatRegistrationNumberYesNoController.onPageLoad(index.toString).url
+
+  private val userAnswersWithoutVatAnswer =
+    emptyUserAnswers
+      .set(PartnerDetailsAddPartnerCompletedPage(index), false)
+      .success
+      .value
+      .set(PartnerDetailsMgdRegNumberPage(index), "123456789")
+      .success
+      .value
+      .set(PartnerDetailsMgdRegNumberPage(businessNumber), "123456789")
+      .success
+      .value
+
+  private val userAnswersWithVatAnswer =
+    userAnswersWithoutVatAnswer
+      .set(PartnerDetailsVatRegistrationNumberYesNoPage(index), true)
+      .success
+      .value
+
+  "VatRegistrationNumberYesNoController Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswersWithoutVatAnswer)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(GET, vatRegistrationNumberYesNoRoute)
+
+        val result =
+          route(application, request).value
+
+        val view =
+          application.injector.instanceOf[PartnerDetailsVatRegistrationNumberYesNoView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual
+          view(
+            form,
+            index.toString,
+            NormalMode
+          )(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswersWithVatAnswer)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(GET, vatRegistrationNumberYesNoRoute)
+
+        val view =
+          application.injector.instanceOf[PartnerDetailsVatRegistrationNumberYesNoView]
+
+        val result =
+          route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual
+          view(
+            form.fill(true),
+            index.toString,
+            NormalMode
+          )(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when true is submitted" in {
+
+      val mockSessionRepository =
+        mock[SessionRepository]
+
+      when(mockSessionRepository.set(any()))
+        .thenReturn(Future.successful(true))
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswersWithoutVatAnswer))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, vatRegistrationNumberYesNoRoute)
+            .withFormUrlEncodedBody(("value", "true"))
+
+        val result =
+          route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+
+        val expectedAnswers =
+          userAnswersWithoutVatAnswer
+            .set(PartnerDetailsVatRegistrationNumberYesNoPage(index), true)
+            .success
+            .value
+        verify(mockSessionRepository).set(expectedAnswers)
+      }
+    }
+
+    "must redirect to the next page when false is submitted" in {
+
+      val mockSessionRepository =
+        mock[SessionRepository]
+
+      when(mockSessionRepository.set(any()))
+        .thenReturn(Future.successful(true))
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswersWithoutVatAnswer))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, vatRegistrationNumberYesNoRoute)
+            .withFormUrlEncodedBody(("value", "false"))
+
+        val result =
+          route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+
+        val expectedAnswers =
+          userAnswersWithoutVatAnswer
+            .set(PartnerDetailsVatRegistrationNumberYesNoPage(index), false)
+            .success
+            .value
+        verify(mockSessionRepository).set(expectedAnswers)
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswersWithoutVatAnswer)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, vatRegistrationNumberYesNoRoute)
+            .withFormUrlEncodedBody(("value", ""))
+
+        val boundForm =
+          form.bind(Map("value" -> ""))
+
+        val view =
+          application.injector.instanceOf[PartnerDetailsVatRegistrationNumberYesNoView]
+
+        val result =
+          route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual
+          view(
+            boundForm,
+            index.toString,
+            NormalMode
+          )(request, messages(application)).toString
+      }
+    }
+
+  }
+}
