dd73e14b099fc7617783a98cb7f2be101289f630
AnotherMattWright <270914550+AnotherMattWright@users.noreply.github.com>
2026-09-24 13:38:53 +0100

    fixed assoc reg spec (side mission)

--- test/controllers/AssociatedRegNumberControllerSpec.scala
+++ test/controllers/AssociatedRegNumberControllerSpec.scala
@@ -24,7 +24,7 @@ import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{verify, when}
 import org.scalatestplus.mockito.MockitoSugar
-import pages.tradingdetails.associatedregnumbers.{AssociatedRegNumberPage, AssociatedRegistrationNumbersPage}
+import pages.tradingdetails.associatedregnumbers.{AssociatedRegistrationNumbersPage, ChosenAssociatedRegNumberPage}
 import pages.tradingdetails.{MgdTradeDetailsSectionPage, TradingDetailsChangesPage}
 import play.api.inject.bind
 import play.api.test.FakeRequest
@@ -68,7 +68,7 @@ class AssociatedRegNumberControllerSpec extends SpecBase with MockitoSugar {
           .set(MgdTradeDetailsSectionPage, mgdRegNum)
           .success
           .value
-          .set(AssociatedRegNumberPage, "answer")
+          .set(ChosenAssociatedRegNumberPage, "answer")
           .success
           .value
 
